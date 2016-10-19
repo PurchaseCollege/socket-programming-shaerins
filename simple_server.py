28668385931022dc83f3c5fee9999e5f932979a8
Sharon <shaerins@gmail.com>
2016-10-19 11:14:38 -0400

    slicing algorithm

--- simple_server.py
+++ simple_server.py
@@ -12,26 +12,37 @@ my_socket.bind(("localhost", 3000))
 # 3. Begin "listening" on the socket
 my_socket.listen(5)
 
-
 # 4. Begin "accepting" client connections
 conn, addr = my_socket.accept()
 
-while(True):
+def openFile(filename):
+    return open(filename, "rb").read()
+
+htmlf = open("inx.html", "rb").read()
 
 # 5. Receive some data (up to 1024 bytes) FROM the client
-    data = ''
-    data = conn.recv(1024)
-    data_string = str(data)
+data = ''
+data = conn.recv(1024)
+data_string = str(data)
+get_string = str(data)
 
-    if not data: break
-    conn.sendall(data)
+if not data: break
+conn.sendall(data)
 
-    for i in range(0, len(data)):
-        if(data_string[i] == "\\" and data_string[i+1] == "r" and data_string[i+2] == "\\" and data_string[i+3] == "n"):
-            x = data_string[2:i]
-            print(x)
-            break
+for i in range(0, len(data_string)):
+    if(data_string[i:i+4] == "\\r\\n"):
+        get_string = data_string[2:i]
+        print(x)
+        break
 
+if(get_string[0:3] == "GET"):
+    conn.sendall(htmlf)
+
+fileName = ""
+for i in range(4, len(get_string)):
+    if(get_string[i] == " "):
+        fileName = get_string[4:i]
+        break
 
 # 6. Send some data back TO the client
 #conn.sendall("Hello Web Client!")
